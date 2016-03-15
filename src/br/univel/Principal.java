1abe8fa4c1ffe485e83b2b5ab4942079487ed33e
Carlos Luiz <carloslouterio15@gmail.com>
2016-03-15 20:10:33 -0300

    Usando Orientação a objetos

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -3,7 +3,7 @@ package br.univel;
 public class Principal {
 	
 	public static void main (String[] args){
-		System.out.println("Ola Mundo");
+		new message().hello();
 		System.out.println("E Tóis");
 
 	}
